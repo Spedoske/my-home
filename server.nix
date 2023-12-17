c92cfa26eb08394a72bba42785b9e91a2e1c5d59
kashun <kashun@berkeley.edu>
2023-12-16 18:55:19 -0800

    add vcode-server

--- server.nix
+++ server.nix
@@ -1,6 +1,15 @@
 { pkgs, isDesktop, ... }:
 if isDesktop then { } else
 {
+  imports = [
+    "${fetchTarball {
+      url =  "https://github.com/msteen/nixos-vscode-server/tarball/master";
+      sha256 = "sha256:0sz8njfxn5bw89n6xhlzsbxkafb6qmnszj4qxy2w0hw2mgmjp829";
+     } }/modules/vscode-server/home.nix"
+  ];
+
+  services.vscode-server.enable = true;
+
   home.packages = with pkgs; [
     iftop
   ];
